9e2ab5bc6a95c1446ae6a13c4d3fdbed11c99f4c
Waldo1637 <hoffman.timothy09@gmail.com>
2016-02-18 12:09:55 -0600

    Formatting

--- PlayInAgent/src/de/bodden/tamiflex/playin/Agent.java
+++ PlayInAgent/src/de/bodden/tamiflex/playin/Agent.java
@@ -1,13 +1,13 @@
-/*******************************************************************************
+/* *****************************************************************************
  * Copyright (c) 2010 Eric Bodden.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Eric Bodden - initial API and implementation
- ******************************************************************************/
+ *****************************************************************************/
 package de.bodden.tamiflex.playin;
 
 import java.io.File;
@@ -25,134 +25,140 @@ import java.util.Properties;
 import de.bodden.tamiflex.normalizer.Hasher;
 
 /**
- * This agent registers a {@link ClassReplacer} as a class-file transformer. 
+ * This agent registers a {@link ClassReplacer} as a class-file transformer.
  */
 public class Agent {
-	
-	public final static String PKGNAME = Agent.class.getPackage().getName().replace('.', '/');
 
-	private static String inPath = "out";
-	private static boolean verbose = false;
+    public final static String PKGNAME = Agent.class.getPackage().getName().replace('.', '/');
 
-	public static void premain(String agentArgs, Instrumentation inst) throws IOException, ClassNotFoundException, UnmodifiableClassException, URISyntaxException, IllegalClassFormatException {
-		
-		System.out.println("=======================================================");
-		System.out.println("TamiFlex Play-In Agent Version "+Agent.class.getPackage().getImplementationVersion());
-		loadProperties();
-		
-		final ClassReplacer replacer = new ClassReplacer(inPath,verbose);
-		inst.addTransformer(replacer,true);
-		
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			@Override
-			public void run() {
-				System.out.println("\n=======================================================");
-				System.out.println("TamiFlex Play-In Agent Version "+Agent.class.getPackage().getImplementationVersion());
-				System.out.println("Replaced "+replacer.numSuccess+" out of "+replacer.numInvoked+" classes.");
-				System.out.println("=======================================================");
-			}
-		});
-		
-		System.out.println("=======================================================");
+    private static String inPath = "out";
+    private static boolean verbose = false;
 
-		for (Class<?> c : inst.getAllLoadedClasses()) {
-			if(inst.isModifiableClass(c)) {
-				inst.retransformClasses(c);
-			} else if(verbose) {
-				//warn if there is a class that we cannot re-transform, except for classes that resemble primitive types,
-				//arrays or are in java.lang
-				if(!c.isPrimitive() && !c.isArray() && (c.getPackage()==null || !c.getPackage().getName().startsWith("java.lang"))){
-					System.out.println("WARNING: Cannot replace class "+c.getName());
-				}
-			}
-		}
-	}
-	
-	private static void loadProperties() {
-		String propFileName = "pia.properties";
-		String userPropFilePath = System.getProperty("user.home")+File.separator+".tamiflex"+File.separator+propFileName;
-		copyPropFileIfMissing(userPropFilePath);
-		String[] paths = { propFileName, userPropFilePath };
-		InputStream is = null;
-		File foundFile= null;
-		for (String path : paths) {
-			File file = new File(path);
-			if(file.exists() && file.canRead()) {
-				try {
-					is = new FileInputStream(file);
-					foundFile = file;
-					break;
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-			} 
-		}
-		if(is==null) throw new InternalError("No properties files found!");
-		
-		Properties props =  new Properties();
-		try {
-			props.load(is);
+    public static void premain(String agentArgs, Instrumentation inst) throws IOException, ClassNotFoundException, UnmodifiableClassException, URISyntaxException, IllegalClassFormatException {
 
-			if(!props.containsKey("quiet") || !props.get("quiet").equals("true")) {
-				String path = (foundFile!=null) ? foundFile.getAbsolutePath() : "<JAR FILE>!/"+propFileName;
-				System.out.println("Loaded properties from "+path);
-			}
-			if(props.get("dontNormalize").equals("true"))
-				Hasher.dontNormalize();
-			if(props.get("verbose").equals("true"))
-				verbose = true;
-			if(props.containsKey("inDir"))
-				inPath = (String) props.get("inDir"); 
+        System.out.println("=======================================================");
+        System.out.println("TamiFlex Play-In Agent Version " + Agent.class.getPackage().getImplementationVersion());
+        loadProperties();
 
-		} catch (IOException e) {
-			throw new InternalError("Error loading default properties file: "+e.getMessage()); 
-		}		
-	}
-	
-	//COPIED from POA
-	private static void copyPropFileIfMissing(String userPropFilePath) {
-		File f = new File(userPropFilePath);
-		if(!f.exists()) {
-			File dir = f.getParentFile();
-			if(!dir.exists()) dir.mkdirs();
-			try {
-				FileOutputStream fos = new FileOutputStream(f);
-				InputStream is = Agent.class.getClassLoader().getResourceAsStream(f.getName());
-				if(is==null) {
-					throw new InternalError("No default properties file found in agent JAR file!");
-				}
-				int i;
-				while((i=is.read())!=-1) {
-					fos.write(i);
-				}
-				fos.close();
-				is.close();				
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+        final ClassReplacer replacer = new ClassReplacer(inPath, verbose);
+        inst.addTransformer(replacer, true);
 
-	
-	public static void main(String[] args) {
-		usage();
-	}
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.out.println("\n=======================================================");
+                System.out.println("TamiFlex Play-In Agent Version " + Agent.class.getPackage().getImplementationVersion());
+                System.out.println("Replaced " + replacer.numSuccess + " out of " + replacer.numInvoked + " classes.");
+                System.out.println("=======================================================");
+            }
+        });
 
-	private static void usage() {
-		System.out.println("============================================================");
-		System.out.println("TamiFlex Play-In Agent Version "+Agent.class.getPackage().getImplementationVersion());
-		System.out.println(DISCLAIMER);
-		System.out.println("============================================================");
-		System.exit(1);
-	}
-	
-	private final static String DISCLAIMER=
-		"Copyright (c) 2010 Eric Bodden.\n" +
-		"\n" +
-		"DISCLAIMER: USE OF THIS SOFTWARE IS AT OWN RISK.\n" +
-		"\n" +
-		"All rights reserved. This program and the accompanying materials\n" +
-		"are made available under the terms of the Eclipse Public License v1.0\n" +
-		"which accompanies this distribution, and is available at\n" +
-		"http://www.eclipse.org/legal/epl-v10.html";
+        System.out.println("=======================================================");
+
+        for (Class<?> c : inst.getAllLoadedClasses()) {
+            if (inst.isModifiableClass(c)) {
+                inst.retransformClasses(c);
+            } else if (verbose) {
+                //warn if there is a class that we cannot re-transform, except for classes that resemble primitive types,
+                //arrays or are in java.lang
+                if (!c.isPrimitive() && !c.isArray() && (c.getPackage() == null || !c.getPackage().getName().startsWith("java.lang"))) {
+                    System.out.println("WARNING: Cannot replace class " + c.getName());
+                }
+            }
+        }
+    }
+
+    private static void loadProperties() {
+        String propFileName = "pia.properties";
+        String userPropFilePath = System.getProperty("user.home") + File.separator + ".tamiflex" + File.separator + propFileName;
+        copyPropFileIfMissing(userPropFilePath);
+        String[] paths = {propFileName, userPropFilePath};
+        InputStream is = null;
+        File foundFile = null;
+        for (String path : paths) {
+            File file = new File(path);
+            if (file.exists() && file.canRead()) {
+                try {
+                    is = new FileInputStream(file);
+                    foundFile = file;
+                    break;
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if (is == null) {
+            throw new InternalError("No properties files found!");
+        }
+
+        Properties props = new Properties();
+        try {
+            props.load(is);
+
+            if (!props.containsKey("quiet") || !props.get("quiet").equals("true")) {
+                String path = (foundFile != null) ? foundFile.getAbsolutePath() : "<JAR FILE>!/" + propFileName;
+                System.out.println("Loaded properties from " + path);
+            }
+            if (props.get("dontNormalize").equals("true")) {
+                Hasher.dontNormalize();
+            }
+            if (props.get("verbose").equals("true")) {
+                verbose = true;
+            }
+            if (props.containsKey("inDir")) {
+                inPath = (String) props.get("inDir");
+            }
+
+        } catch (IOException e) {
+            throw new InternalError("Error loading default properties file: " + e.getMessage());
+        }
+    }
+
+    //COPIED from POA
+    private static void copyPropFileIfMissing(String userPropFilePath) {
+        File f = new File(userPropFilePath);
+        if (!f.exists()) {
+            File dir = f.getParentFile();
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+            try {
+                FileOutputStream fos = new FileOutputStream(f);
+                InputStream is = Agent.class.getClassLoader().getResourceAsStream(f.getName());
+                if (is == null) {
+                    throw new InternalError("No default properties file found in agent JAR file!");
+                }
+                int i;
+                while ((i = is.read()) != -1) {
+                    fos.write(i);
+                }
+                fos.close();
+                is.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        usage();
+    }
+
+    private static void usage() {
+        System.out.println("============================================================");
+        System.out.println("TamiFlex Play-In Agent Version " + Agent.class.getPackage().getImplementationVersion());
+        System.out.println(DISCLAIMER);
+        System.out.println("============================================================");
+        System.exit(1);
+    }
+
+    private final static String DISCLAIMER
+            = "Copyright (c) 2010 Eric Bodden.\n"
+            + "\n"
+            + "DISCLAIMER: USE OF THIS SOFTWARE IS AT OWN RISK.\n"
+            + "\n"
+            + "All rights reserved. This program and the accompanying materials\n"
+            + "are made available under the terms of the Eclipse Public License v1.0\n"
+            + "which accompanies this distribution, and is available at\n"
+            + "http://www.eclipse.org/legal/epl-v10.html";
 }
--- PlayOutAgent/src/de/bodden/tamiflex/playout/ClassDumper.java
+++ PlayOutAgent/src/de/bodden/tamiflex/playout/ClassDumper.java
@@ -1,13 +1,13 @@
-/*******************************************************************************
+/* *****************************************************************************
  * Copyright (c) 2010 Eric Bodden.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Eric Bodden - initial API and implementation
- ******************************************************************************/
+ *****************************************************************************/
 package de.bodden.tamiflex.playout;
 
 import static de.bodden.tamiflex.normalizer.Hasher.containsGeneratedClassName;
@@ -32,102 +32,107 @@ import de.bodden.tamiflex.normalizer.NameExtractor;
 
 public class ClassDumper implements ClassFileTransformer {
 
-	protected final File outDir; 
-	
-	/**
-	 * It is important that this be a <i>linked</i> hash map because we need to generate hash numbers
-	 * for the classes in the order in which they are loaded. This is because a generated class <i>a</i> may reference
-	 * other generated classes, and when determining a hash code for <i>a</i>, the hash code for those
-	 * referenced classes must already have been computed.
-	 */
-	protected final LinkedHashMap<String,byte[]> classNameToBytes = new LinkedHashMap<String, byte[]>();
+    protected final File outDir;
 
-	private final boolean verbose;
+    /**
+     * It is important that this be a <i>linked</i> hash map because we need to
+     * generate hash numbers for the classes in the order in which they are
+     * loaded. This is because a generated class <i>a</i> may reference other
+     * generated classes, and when determining a hash code for <i>a</i>, the
+     * hash code for those referenced classes must already have been computed.
+     */
+    protected final LinkedHashMap<String, byte[]> classNameToBytes = new LinkedHashMap<String, byte[]>();
 
-	private final boolean dontReallyDump;
-	
-	public int newClasses;
-	
-	public ClassDumper(File outDir, boolean dontReallyDump, boolean verbose) {
-		this.outDir = outDir;
-		this.dontReallyDump = dontReallyDump;
-		this.verbose = verbose;
-	}
+    private final boolean verbose;
 
-	public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
-		if(className==null) {
-			className = NameExtractor.extractName(classfileBuffer);
-		}
-		if(hasShutDown) return null;
-		if(className.startsWith(Agent.PKGNAME)) return null;
-		
-		byte[] oldBytes;
-		synchronized (this) {
-			oldBytes = classNameToBytes.put(className, classfileBuffer);
-		}
+    private final boolean dontReallyDump;
 
-		if(verbose && oldBytes!=null && !Arrays.equals(classfileBuffer, oldBytes)) {
-			System.err.println("WARNING: There exist two different classes with name "+className);
-		}
+    public int newClasses;
 
-		return null;
-	}
-	
-	public void writeClassesToDisk() {
-		synchronized (this) {
-			Set<Entry<String, byte[]>> entrySet = classNameToBytes.entrySet();
-			for (Map.Entry<String, byte[]> entry: entrySet) {
-				String className = entry.getKey();
-				byte[] classfileBuffer = entry.getValue();
-		
-				if(containsGeneratedClassName(className)) {
-					generateHashNumber(className, classfileBuffer);
-					className = hashedClassNameForGeneratedClassName(className);
-					classfileBuffer = replaceGeneratedClassNamesByHashedNames(classfileBuffer);
-				}
-	
-				if(dontReallyDump) continue; //don't dump
-				
-				File localOutDir = outDir;
-				
-				localOutDir.mkdirs();
-				
-				String simpleName = className;
-				
-				if(className.contains("/")) {
-					String packageName = className.substring(0,className.lastIndexOf('/'));
-					simpleName = className.substring(className.lastIndexOf('/')+1);
-	
-					localOutDir = new File(localOutDir,packageName);
-					localOutDir.mkdirs();
-				}
-				
-				String fileName = simpleName+".class";
-				
-				File outFile = new File(localOutDir, fileName);
-				if(outFile.exists()) {
-					outFile.delete();
-				} else {
-					newClasses++;
-				}
-				FileOutputStream fos = null;
-				try {
-					outFile.createNewFile();
-					fos = new FileOutputStream(outFile);
-					fos.write(classfileBuffer);
-				} catch (IOException e) {
-					e.printStackTrace();
-				} finally {
-					if(fos!=null) {
-						try {
-							fos.close();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-				}
-			}
-		}
-	}
+    public ClassDumper(File outDir, boolean dontReallyDump, boolean verbose) {
+        this.outDir = outDir;
+        this.dontReallyDump = dontReallyDump;
+        this.verbose = verbose;
+    }
 
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+        if (className == null) {
+            className = NameExtractor.extractName(classfileBuffer);
+        }
+        if (hasShutDown) {
+            return null;
+        }
+        if (className.startsWith(Agent.PKGNAME)) {
+            return null;
+        }
+
+        byte[] oldBytes;
+        synchronized (this) {
+            oldBytes = classNameToBytes.put(className, classfileBuffer);
+        }
+
+        if (verbose && oldBytes != null && !Arrays.equals(classfileBuffer, oldBytes)) {
+            System.err.println("WARNING: There exist two different classes with name " + className);
+        }
+
+        return null;
+    }
+
+    public void writeClassesToDisk() {
+        synchronized (this) {
+            Set<Entry<String, byte[]>> entrySet = classNameToBytes.entrySet();
+            for (Map.Entry<String, byte[]> entry : entrySet) {
+                String className = entry.getKey();
+                byte[] classfileBuffer = entry.getValue();
+
+                if (containsGeneratedClassName(className)) {
+                    generateHashNumber(className, classfileBuffer);
+                    className = hashedClassNameForGeneratedClassName(className);
+                    classfileBuffer = replaceGeneratedClassNamesByHashedNames(classfileBuffer);
+                }
+
+                if (dontReallyDump) {
+                    continue; //don't dump
+                }
+                File localOutDir = outDir;
+
+                localOutDir.mkdirs();
+
+                String simpleName = className;
+
+                if (className.contains("/")) {
+                    String packageName = className.substring(0, className.lastIndexOf('/'));
+                    simpleName = className.substring(className.lastIndexOf('/') + 1);
+
+                    localOutDir = new File(localOutDir, packageName);
+                    localOutDir.mkdirs();
+                }
+
+                String fileName = simpleName + ".class";
+
+                File outFile = new File(localOutDir, fileName);
+                if (outFile.exists()) {
+                    outFile.delete();
+                } else {
+                    newClasses++;
+                }
+                FileOutputStream fos = null;
+                try {
+                    outFile.createNewFile();
+                    fos = new FileOutputStream(outFile);
+                    fos.write(classfileBuffer);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (fos != null) {
+                        try {
+                            fos.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
